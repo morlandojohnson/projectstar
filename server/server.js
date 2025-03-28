c7c7e0721b91d5a63754c55e14ad8eeb5b91a6e7
Morlando Johnson <97807888+Lando241@users.noreply.github.com>
2025-03-28 17:06:09 -0400

    updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
